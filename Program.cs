5b7bc997270e9933e4abb033d2d85ab2d2274eff
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-01-21 13:01:29 -0800

    added notes

--- Program.cs
+++ Program.cs
@@ -6,16 +6,16 @@ namespace COMP003A.LectureActivity2
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the first number: ");
-            string input1 = Console.ReadLine();
+            Console.Write("Enter the first number: "); //ask for # 
+            string input1 = Console.ReadLine();  //read #
 
             Console.Write("Enter the second number: ");
             string input2 = Console.ReadLine();
 
-            double a = double.Parse(input1);
+            double a = double.Parse(input1); //convert string from above to #
             double b = double.Parse(input2);
 
-            double sum = a + b;
+            double sum = a + b; //use # for calcs
             double average = (a + b) / 2.0;
         }
     }
