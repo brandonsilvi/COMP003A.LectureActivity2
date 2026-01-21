6015239f12aaee455fc33818bb827f54ca5083dd
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-01-21 13:00:16 -0800

    added using system and namespace

--- Program.cs
+++ Program.cs
@@ -1,17 +1,22 @@
-﻿internal class Program
+﻿using System;
+
+namespace COMP003A.LectureActivity2
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        Console.Write("Enter the first number: ");
-        string input1 = Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            string input1 = Console.ReadLine();
 
-        Console.Write("Enter the second number: ");
-        string input2 = Console.ReadLine();
+            Console.Write("Enter the second number: ");
+            string input2 = Console.ReadLine();
 
-        double a = double.Parse(input1);
-        double b = double.Parse(input2);
+            double a = double.Parse(input1);
+            double b = double.Parse(input2);
 
-        double sum = a + b;
-        double average = (a + b) / 2.0;
-    }    
+            double sum = a + b;
+            double average = (a + b) / 2.0;
+        }
+    }
 }
